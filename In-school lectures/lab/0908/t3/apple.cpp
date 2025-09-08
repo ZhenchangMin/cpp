2123b2d7378818b34163b3285d126df83cf8b91a
241880334 <241880334@smail.nju.edu.cn>
2025-09-08 19:42:52 +0800

    UP

--- In-school lectures/lab/0908/t3/apple.cpp
+++ In-school lectures/lab/0908/t3/apple.cpp
@@ -5,9 +5,9 @@ vector<int> current;
 
 void backtrack(int m_left, int index, int max_n, int prev)
 {
-    if (index == max_n)
+    if (index == max_n)//n个位置占满了
     {
-        if (m_left == 0)
+        if (m_left == 0)//如果m已经被分配完
         {
             for (int i = 0; i < max_n; ++i)
             {
@@ -20,13 +20,13 @@ void backtrack(int m_left, int index, int max_n, int prev)
         return;
     }
 
-    if (m_left == 0)
+    if (m_left == 0)//如果m已经被分配完，并且n没有被占满
     {
-        current[index] = 0;
+        current[index] = 0;//用递归来把后面的位置都赋值为0，因为后面的递归都会进入这一个if分支，而index不断加一，直到n个位置填满进入上面的分支，输出
         backtrack(0, index + 1, max_n, 0); 
         return;
     }
-
+//其余的情况，如果m没有被分配完，尝试所有剩下的数字，对每个数字num采用递归，相当于现在有m_left-num个苹果分给n_left-1个盘子，n_left是剩下的n，这里通过更新index来实现
     for (int num = min(m_left, prev); num >= 0; --num)
     {
         current[index] = num;
